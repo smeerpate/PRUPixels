7b7abd9847bff6def837003b0a90b528a5215d1c
smeerpate <51826342+smeerpate@users.noreply.github.com>
2025-11-13 09:44:08 +0100

    cleanup and loop playback

--- player.c
+++ player.c
@@ -125,97 +125,62 @@ int main()
 	printf("[INFO] Memory mapped at address %p.\n", pruSharedMemPointer); 
     fflush(stdout);
 
-	playbackStartTime = av_gettime_relative() / 1000000.0; // huidige tijd in seconden (nodig voor synchronisatie
-	
-	AVPacket packet; // AVPacket bevat: Gecodeerde data en Metadata zoals stream_index, pts, dts, enz.
-    while (av_read_frame(fmt_ctx, &packet) >= 0) // lees MP4-bestandpakketten zolang er zijn
+
+	while (1) // loop playback
 	{
-		if (packet.stream_index == video_stream_index) // ingelezen pakket (AVPacket) behoort tot de videostream?
+		playbackStartTime = av_gettime_relative() / 1000000.0; // huidige tijd in seconden (nodig voor synchronisatie
+		
+		AVPacket packet; // AVPacket bevat: Gecodeerde data en Metadata zoals stream_index, pts, dts, enz.
+		while (av_read_frame(fmt_ctx, &packet) >= 0) // lees MP4-bestandpakketten zolang er zijn
 		{
-			if (avcodec_send_packet(codec_ctx, &packet) == 0) // het versturen van een gecodeerd pakket naar de decoder succesvol is verlopen?
+			if (packet.stream_index == video_stream_index) // ingelezen pakket (AVPacket) behoort tot de videostream?
 			{
-				// Nu kunnen we proberen een frame te ontvangen
-				while (avcodec_receive_frame(codec_ctx, frame) == 0)
+				if (avcodec_send_packet(codec_ctx, &packet) == 0) // het versturen van een gecodeerd pakket naar de decoder succesvol is verlopen?
 				{
-					sws_scale(sws_ctx, (uint8_t const * const *)frame->data, frame->linesize, 0, codec_ctx->height, rgb_frame->data, rgb_frame->linesize); // scale het frame en sla op in RBG buffer
-/* 					get_pixel_rgb(rgb_frame, 50, 30, &RGB);
-					((unsigned long *) virt_addr)[0] = RGB;
-					get_pixel_rgb(rgb_frame, 51, 30, &RGB);
-					((unsigned long *) virt_addr)[1] = RGB;
-					get_pixel_rgb(rgb_frame, 52, 30, &RGB);
-					((unsigned long *) virt_addr)[2] = RGB;
-					get_pixel_rgb(rgb_frame, 53, 30, &RGB);
-					((unsigned long *) virt_addr)[3] = RGB; */
-					for(int i=0; i<NPIXELS; i++)
+					// Nu kunnen we proberen een frame te ontvangen
+					while (avcodec_receive_frame(codec_ctx, frame) == 0)
 					{
-						get_pixel_rgb(rgb_frame, i%OUTWIDTH, 30, &RGB);
-						((unsigned long *) pruSharedMemPointer)[i] = RGB;
+						sws_scale(sws_ctx, (uint8_t const * const *)frame->data, frame->linesize, 0, codec_ctx->height, rgb_frame->data, rgb_frame->linesize); // scale het frame en sla op in RBG buffer
+						
+						for(int i=0; i<NPIXELS; i++)
+						{
+							get_pixel_rgb(rgb_frame, i%OUTWIDTH, 30, &RGB);
+							((unsigned long *) pruSharedMemPointer)[i] = RGB;
+						}
+						
+						// wacht tot het tijd is om de volgende frame af te spelen
+						frameTimestamp = frame->pts * av_q2d(video_stream->time_base); // frame timestamp in seconden
+						currentTime = (av_gettime_relative() / 1000000.0) - playbackStartTime;
+						if (frameTimestamp > currentTime)
+						{
+							double delay = frameTimestamp - currentTime;
+							av_usleep((int64_t)(delay * 1000000));
+						}
+						else
+						{
+							printf("[WARNING] video loopt niet in sync!\n");
+							fflush(stdout);
+						}
+						
+						//printf("%d ", frame->linesize[0]);
+						//fflush(stdout);
 					}
-					
-					// wacht tor het tijd is om de volgende frame af te spelen
-					frameTimestamp = frame->pts * av_q2d(video_stream->time_base); // frame timestamp in seconden
-					currentTime = (av_gettime_relative() / 1000000.0) - playbackStartTime;
-					if (frameTimestamp > currentTime)
-					{
-						double delay = frameTimestamp - currentTime;
-						av_usleep((int64_t)(delay * 1000000));
-					}
-					else
-					{
-						printf("[WARNING] video loopt niet in sync!\n");
-						fflush(stdout);
-					}
-					
-					//printf("%d ", frame->linesize[0]);
-					//fflush(stdout);
 				}
 			}
+			av_packet_unref(&packet); // AVPacket geheugen vrijmaken, kan >200kB zijn
 		}
-		av_packet_unref(&packet); // AVPacket geheugen vrijmaken, kan >200kB zijn
+		
+		// FFMPEG Cleanup
+		av_free(pixelBuffer);
+		av_frame_free(&frame);
+		av_frame_free(&rgb_frame);
+		sws_freeContext(sws_ctx);
+		avcodec_free_context(&codec_ctx);
+		avformat_close_input(&fmt_ctx);
+
+		
+		printf("[INFO] Video geschreven naar PRU shared memory.\n");
 	}
-	
-	// Cleanup
-    av_free(pixelBuffer);
-    av_frame_free(&frame);
-    av_frame_free(&rgb_frame);
-    sws_freeContext(sws_ctx);
-    avcodec_free_context(&codec_ctx);
-    avformat_close_input(&fmt_ctx);
-
-	
-/*      // Open /dev/mem
-    int mem_fd = open("/dev/mem", O_RDWR | O_SYNC);
-    if (mem_fd < 0)
-	{
-        perror("[ERROR] open");
-        return 1;
-    }
-	printf("[INFO] /dev/mem opened...\n"); 
-    fflush(stdout);
-	
-	
-    // Memory map PRU shared memory
-    pruSharedMemPointer = mmap(NULL, PRU_SHARED_MEM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, mem_fd, PRU_SHARED_MEM_PHYS & ~MAP_MASK);
-    if (pruSharedMemPointer == MAP_FAILED)
-	{
-        perror("[ERROR] mmap");
-        close(mem_fd);
-        return 1;
-    }
-	printf("[INFO] Memory mapped at address %p.\n", pruSharedMemPointer); 
-    fflush(stdout);
-
-	
-	// Schrijf pixels naar PRU shared memory
-	virt_addr = pruSharedMemPointer;
-    for (int i = 0; i < NPIXELS; i++)
-	{
-		((unsigned long *) virt_addr)[i] = pixels[i];  // RGBA pixel
-		//printf("0x%X ", pixels[i]);
-    } */
-	
-	
-	printf("[INFO] Beeldje geschreven naar PRU shared memory.\n");
 
     munmap(pruSharedMemPointer, PRU_SHARED_MEM_SIZE);
     close(mem_fd);
