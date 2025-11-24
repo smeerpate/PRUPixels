b76ef42eb431bd6f9f5b08d20be5419831d909ba
smeerpate <51826342+smeerpate@users.noreply.github.com>
2025-11-24 14:06:47 +0100

    Update pixelLUT.c

--- pixelLUT.c
+++ pixelLUT.c
@@ -1,5 +1,6 @@
 #include "pixelLUT.h"
 
+// In this table: index is the inline number of the pixel (i.e. first pixel is index 0) value is the pixel in the image modulated with line length/3.
 const int pixelLookupTable[TABLESIZE] {
 	0, 0, 0, 0, 0
 };
