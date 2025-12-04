9676cf1e7c192c686ec154ca470414827c3c81ef
smeerpate <51826342+smeerpate@users.noreply.github.com>
2025-12-04 14:01:18 +0100

    Update player.c

--- Player/player.c
+++ Player/player.c
@@ -30,7 +30,7 @@ int main()
             AVStream *videoStream;
             AVFormatContext *fmtCtx = initVideo(filename, &codecCtx, &videoStream);
 			
-			printf("[INFO] Codec context: bitrate:%d, size:(%d x %d).\n", codecCtx->bit_rate, codecCtx->width, codecCtx->height);
+			printf("[INFO] Codec context: bitrate:%d, size:(%d x %d).\n", codecCtx->bit_rate, (*codecCtx)->width, codecCtx->height);
 			printf("[INFO] AV Stream: id:%d, stream index:%d.\n", videoStream->id, videoStream->index);
 			fflush(stdout);
 			
